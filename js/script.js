620259369f71d49b2f2b264df3ab6c73bd17c55d
default <trisha.ray@stu.ocsb.ca>
2025-05-07 17:11:16 +0000

    java script

--- js/script.js
+++ js/script.js
@@ -3,31 +3,19 @@
 // Created by: Trisha Ray
 // Created on: Feb 2025
 // This file contains the JS functions for index.html
-'use strict'
+"use strict"
 
+// This function adds up the natural numbers
 function calculate() {
+  let numberValue = parseFloat(document.getElementById("number").value)
+  let text = ""
+  let number = 0
+
   // input
-  const movieRating = document.getElementById("movieforage").value
-
-  // process
-  if (movieRating >= 17) {
-    // output
-    document.getElementById("results").innerHTML =
-      "You can see R rated movies."
+  for (let naturalNumber = 1; naturalNumber <= numberValue; naturalNumber++) {
+    number += naturalNumber
   }
-
-  else if (movieRating >= 13) {
-    document.getElementById("results").innerHTML =
-      "You can see PG-13 rated movies."
-  }
-
-  else if (movieRating >= 5) {
-    document.getElementById("results").innerHTML =
-      "You can see G and PG rated movies."
-  }
-
-  else {
-    document.getElementById("results").innerHTML =
-      "Uhh, you are to young to see most things."
-  }
+  // output
+  text += "The sum of natural numbers is " + number + "<br>"
+  document.getElementById("results").innerHTML = text
 }
